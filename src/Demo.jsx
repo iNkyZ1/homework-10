60fd7404115838a273ff65cbd7fb4cdaa0cd4f35
Nick <n.t.1996@mail.ru>
2025-11-28 21:08:57 +0400

    feat: add Demo component using useViewportSize hook

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { useViewportSize } from "./hooks/useViewportSize";
 
 function Demo() {
-  return <div>Здесь будет хук useViewportSize</div>;
+  const { height, width } = useViewportSize();
+
+  return (
+    <>
+      <p>
+        Width: {width}, height: {height}
+      </p>
+    </>
+  );
 }
 
 export default Demo;
