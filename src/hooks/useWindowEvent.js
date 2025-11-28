096649b12d4b9137c8380a799268473658f85cd3
Nick <n.t.1996@mail.ru>
2025-11-28 21:07:53 +0400

    feat: implement useWindowEvent hook for window listeners

--- src/hooks/useWindowEvent.js
+++ src/hooks/useWindowEvent.js
@@ -1,5 +1,11 @@
 import { useEffect } from "react";
 
 export function useWindowEvent(type, listener, options) {
-  useEffect(() => {}, [type, listener, options]);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener(type, listener, options);
+
+      return () => window.removeEventListener(type, listener, options);
+    }
+  }, [type, listener]);
 }
